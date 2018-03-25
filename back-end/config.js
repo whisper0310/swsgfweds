989fed659990ca4481a2ab886338e0485183871a
czxrrr <710516537@qq.com>
2018-03-24 22:04:15 -0400

    123

---
+++ back-end/config.js
@@ -0,0 +1,5 @@
+var config = {
+    path: "mongodb://root:admin@ds163796.mlab.com:63796/eat"
+};
+
+module.exports = config;
--- back-end/server.js
+++ back-end/server.js
@@ -2,10 +2,8 @@ var express= require("express");
 var app = express();
 var restRouter = require("./routes/rest.js");
 var mongoose = require("mongoose");
-var config= require("./config");
-
-
-mongoose.connect(config.mongodbpath);
+var config = require("./config");
+mongoose.connect(config.path);
 
 
 app.use(function(req, res, next) {
