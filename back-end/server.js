d2d4fc8b566334263de6b0605aedad163529ab4d
czxrrr <710516537@qq.com>
2018-04-04 21:25:30 -0400

    123

--- back-end/server.js
+++ back-end/server.js
@@ -8,7 +8,6 @@ var io = require('socket.io')(server);
 
 mongoose.connect(config.path);
 
-// server.listen(80);
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -19,24 +18,7 @@ app.use("/api/v1", restRouter);
 var UserController = require('./user/UserController');
 app.use('/api/v1/auth', UserController);
 
-// io.on('connection', function (socket) {
-//     socket.emit('news', { hello: 'world' });
-//     socket.on('my other event', function (data) {
-//         console.log(data);
-//     });
-// });
-// var cart={ recipes:[] };
-// io.on('connection', function (socket) {
-//
-//     // socket.emit('news', { hello: 'world' });
-//     socket.on('add', function (data) {
-//         console.log(data);
-//         cart.recipes.push(data);
-//         cart.recipes.push(data);
-//     });
-//     socket.emit('cart', cart);
-//
-// });
+
 
 app.listen(3000, function(){
   console.log("App started listening on port 3000");
